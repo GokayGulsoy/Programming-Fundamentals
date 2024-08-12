e4abaef19658650ed74306d9baf29edb16ee467e
GÖKAY GÜLSOY <111280608+GokayGulsoy@users.noreply.github.com>
2024-08-12 12:31:54 +0300

    homework2 uploaded

---
+++ Homeworks/G31_CENG211_HW2/HOMEWORK2/src/FinancialStatus.java
@@ -0,0 +1,58 @@
+public class FinancialStatus {
+         
+	// instance variables of the FinancialStatus class
+	private String income;
+	private String savings;
+	
+    // constructor for FinancialStatus class
+	public FinancialStatus(String income,String savings) {
+		
+		this.income = income;
+		this.savings = savings;
+		
+	}
+	
+	// Copy constructor for FinancialStatus class
+	public FinancialStatus(FinancialStatus otherFinancialStatus) {
+		
+		if (otherFinancialStatus == null) {
+			System.out.println("Fatal error object is null");
+			System.exit(0);
+			
+		}
+		
+		else {
+			
+			income = otherFinancialStatus.income;
+			savings = otherFinancialStatus.savings;
+						
+		}
+		
+	}
+	  
+	// getters (accessors) for FinancialStatus class
+	
+	public String getIncome() {
+		return income;
+		
+	}
+	
+	public String getSavings() {
+		return savings;
+		
+	}
+	
+    // setters (mutators) for FinancialStatus class
+	
+	public void setIncome(String income) {
+		this.income = income;
+		
+	}
+	
+	public void setSavings(String savings) {
+		this.savings = savings;
+		
+	}
+	
+	
+}
---
+++ Homeworks/G31_CENG211_HW2/HOMEWORK2/src/Tourist.java
@@ -0,0 +1,35 @@
+public class Tourist extends Applicant {
+
+	 // we do not need specific insance variables to this subclass	
+	
+	// derived Tourist class's constructor
+	public Tourist() {
+		super();
+			
+	}
+	
+	// copy constructor of Tourist class
+	public Tourist(Tourist person) {
+		super(person);
+		
+	}
+	
+	
+	// methods related to application results
+
+	@Override
+	public String getApplication() {
+		return ("Applicant ID: " + getID() + ", " + "Name: " + getName() + ", " + "Visa Type: " + "Toursit" 
+	            + ", " + "Status: " + "Accepted" + ", " + "Visa Duration: " + getVisaDuration());		                             
+		
+	}
+	
+	
+	// overloaded version of the getApplication method in case of rejection
+	public String getApplication(String rejectionReason) {
+		return ("Applicant ID: " + getID() + ", " + "Name: " + getName() + ", " + "Visa Type: " + "Toursit" 
+	            + ", " + "Status: " + "Rejected" + ", " + "Reason: " + rejectionReason);		                             
+		
+	}
+		
+}
